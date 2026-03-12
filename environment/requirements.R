fb0a7cb057f24103e4c0464cb282bb6b9904bbb7
Panashe Nyengera <132056524+panawaShe@users.noreply.github.com>
2026-03-12 17:02:38 +0200

    Create requirements.R

---
+++ environment/requirements.R
@@ -0,0 +1,30 @@
+############################################################
+# requirements.R
+# Install required R packages for the malaria ML project
+############################################################
+
+packages <- c(
+  "readxl",
+  "dplyr",
+  "ggplot2",
+  "caret",
+  "car",
+  "corrplot",
+  "randomForest",
+  "e1071",
+  "xgboost",
+  "gbm",
+  "naivebayes",
+  "ROCR",
+  "pROC",
+  "janitor",
+  "smotefamily",
+  "adabag",
+  "glmnet",
+  "rpart",
+  "rpart.plot",
+  "class",
+  "MLmetrics"
+)
+
+install.packages(packages)
