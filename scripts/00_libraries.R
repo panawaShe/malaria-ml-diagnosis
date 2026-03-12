2cd20971e5f4c5af96de40086c25ee0fcd117b83
Panashe Nyengera <132056524+panawaShe@users.noreply.github.com>
2026-03-12 11:36:24 +0200

    Update 00_libraries.R

--- scripts/00_libraries.R
+++ scripts/00_libraries.R
@@ -1,1 +1,29 @@
+############################################################
+# 00_libraries.R
+# Load all required libraries for the malaria ML analysis
+############################################################
 
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(tidyr)
+library(reshape2)
+library(caret)
+library(car)
+library(corrplot)
+library(randomForest)
+library(e1071)
+library(xgboost)
+library(gbm)
+library(naivebayes)
+library(ROCR)
+library(pROC)
+library(janitor)
+library(smoteFamily)
+library(adabag)
+library(glmnet)
+library(rpart)
+library(rpart.plot)
+library(class)
+library(MLmetrics)
